081d5dfc9974545076baf9063e5c758c67fd82a6
0psilon <deniska_demenkov@mail.ru>
2022-12-13 13:03:39 +0400

    Corrected a misspelling

--- service/models.py
+++ service/models.py
@@ -3,7 +3,7 @@ import typing as tp
 from pydantic import BaseModel
 
 # условный список зарегистрированных моделей
-model_names = ['some_model', 'ordinary_popular', 'user_knn']
+model_names = ['some_model', 'ordinary_popular', 'userknn']
 
 
 class Error(BaseModel):
